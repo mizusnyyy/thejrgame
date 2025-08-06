b05c95ed467e8f3a09ef44c7f25b2c3dc530b03e
mizek <91779178+mizusnyyy@users.noreply.github.com>
2025-08-06 17:29:42 +0200

    hpfix

--- scenes/battle/notui.gd
+++ scenes/battle/notui.gd
@@ -33,7 +33,7 @@ func enemyturn():
 	boxcentersizetween.tween_property(
 		box, 
 		"position",          # <- tylko oś X
-		Vector2(320, 256),                # końcowa wartość skali X
+		Vector2(320, 252),                # końcowa wartość skali X
 		0.5                 # czas trwania w sekundach
 	)
 	emit_signal("enemy_turn")

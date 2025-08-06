60cc747b5c5fbb595a691989f66993363228446d
Awruk1 <alanbaran08@gmail.com>
2025-08-06 18:31:42 +0200

    dzien dobry

--- scenes/battle/character_body_2d.gd
+++ scenes/battle/character_body_2d.gd
@@ -23,24 +23,28 @@ var invincible := false
 	get_node("../notui/right arrow")
 ]
 
-
 var current_index := 0
 var current_top_index := 0
 var top=false
 var heart
 
+func _ready():
+	add_to_group("player")
+
 func update_soul_position():
 	soul.global_position = buttons[current_index].global_position
+
 func update_top_soul_position():
 	soul.global_position = topbuttons[current_top_index].global_position
+
 func flash_effect():
 	invincible = true
 	var flash_times = 4
-	if global.health==0:
-		haudio.pitch_scale=0.5
+	if global.health == 0:
+		haudio.pitch_scale = 0.5
 		haudio.play()
 	else:
-		haudio.pitch_scale=1.0
+		haudio.pitch_scale = 1.0
 		haudio.play()
 	for i in flash_times:
 		sprite.modulate.a = 0.3
@@ -48,8 +52,9 @@ func flash_effect():
 		sprite.modulate.a = 1.0
 		await get_tree().create_timer(0.1).timeout
 	invincible = false
-func take_damage(amount,blue=false):
-	if velocity.x==0 and velocity.y==0 and blue:
+
+func take_damage(amount, blue=false):
+	if velocity.x == 0 and velocity.y == 0 and blue:
 		print("bullet oh no im invicible oh no")
 		pass
 	else:
@@ -58,11 +63,12 @@ func take_damage(amount,blue=false):
 		global.health -= amount
 		print("bullet taken:", amount, " → the one holding the gun:", global.health)
 		flash_effect()
+
 func soul_is_alive():
 	return alive
 	
 func _physics_process(delta: float) -> void:
-	if alive and global.current_mode==global.mode.RED:
+	if alive and global.current_mode == global.mode.RED:
 		sprite.play("red")
 		var directionlr := Input.get_axis("left", "right")
 		var directionud := Input.get_axis("up", "down")
@@ -74,7 +80,7 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0.0, SPEED)
 			velocity.y = move_toward(velocity.y, 0.0, SPEED)
 	
-	if alive and global.current_mode==global.mode.BLUE:
+	if alive and global.current_mode == global.mode.BLUE:
 		sprite.play("blue")
 		if not is_on_floor():
 			velocity += get_gravity() * delta
@@ -87,9 +93,10 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if global.health <= 0:
-		alive=false
+		alive = false
 		await get_tree().create_timer(1.5).timeout
 		get_tree().change_scene_to_file("res://scenes/battle/gg/gg.tscn")
 	else:
-		alive=true
+		alive = true
+
 	move_and_slide()
--- scenes/battle/item.gd
+++ scenes/battle/item.gd
@@ -1,11 +1,29 @@
 extends Area2D
 
 @onready var anim = $item
+@onready var info_box = $ColorRect
+@onready var info_label = $ColorRect/Label
+
+var selected = false
+
+func _ready() -> void:
+	info_box.visible = false
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "soul":
+	if body.is_in_group("player"):
 		anim.play("select")
-
+		selected = true
 
 func _on_body_exited(body: Node2D) -> void:
-	anim.play("default")
+	if body.is_in_group("player"):
+		anim.play("default")
+		selected = false
+
+func _process(_delta: float) -> void:
+	if selected and global.current_state == global.state.PLAYER_TURN and Input.is_action_just_pressed("interact"):
+		toggle_item()
+
+func toggle_item():
+	info_box.visible = not info_box.visible

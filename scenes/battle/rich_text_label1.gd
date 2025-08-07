f24e1e5322a3eef0bb878b808bc986c6d4816a22
Igoropolus <i.luniewski@outlook.com>
2025-08-07 16:35:58 +0200

    Ghost_bar

--- scenes/battle/rich_text_label1.gd
+++ scenes/battle/rich_text_label1.gd
@@ -1,5 +1,6 @@
 extends RichTextLabel
-@onready var hp_bar = $"../TextureProgressBar"
+@onready var hp_bar = $".."
+
 func _process(delta: float) -> void:
 	hp_bar.value = global.health
 	text = "%d / %d" %[int(global.health), int(global.maxhealth)]
--- scenes/battle/texture_progress_bar.gd
+++ scenes/battle/texture_progress_bar.gd
@@ -1,6 +1,23 @@
 extends TextureProgressBar
 @onready var hp_bar = $"."
-func ready():
+@onready var timer = $"."
+@onready var ghost_bar = $"../Ghost_bar"
+
+
+func _ready():
 	max_value=global.maxhealth
+	ghost_bar.max_value = max_value
+
+
 func _process(delta: float) -> void:
 	hp_bar.value = global.health
+	if ghost_bar.value > value:
+		ghost_bar.value = lerp(ghost_bar.value, value, delta * 4.0)
+		
+		
+	else:
+		ghost_bar.value = value
+	if abs(ghost_bar.value - value) < 1.0:
+		ghost_bar.value = value
+	
+	

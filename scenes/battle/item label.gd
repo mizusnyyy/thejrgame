70d3cfc20a6a2182e8b7a2aa164825eab52f0bd2
mizek <91779178+mizusnyyy@users.noreply.github.com>
2025-08-07 13:18:49 +0200

    item 1st ver

---
+++ scenes/battle/item label.gd
@@ -0,0 +1,31 @@
+extends RichTextLabel
+
+@onready var notui = $"../../.."
+@onready var label = $"."  # niepotrzebne, można używać self
+@onready var button = $".."
+
+func _ready():
+	visible=false
+	var dir = DirAccess.open("res://scenes/items")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name.ends_with(".tres"):
+				var item = load("res://scenes/items/" + file_name)  # <--- poprawka tutaj
+				if item != null:
+					global.add_item(item)
+			file_name = dir.get_next()
+
+	for item in global.inventory:
+		print(item.name, " - ", item.heal_amount)
+
+func _process(delta: float) -> void:
+	var text = ""
+	for item in global.inventory:
+		text += item.name + "\n"
+	await button.show_items
+	visible=true
+	label.text = text
+	await button.hide_items
+	visible=false
--- scenes/battle/item.gd
+++ scenes/battle/item.gd
@@ -1,27 +1,18 @@
 extends Area2D
-
 @onready var anim = $item
-@onready var info_box = $ColorRect
-@onready var info_label = $ColorRect/Label
 
 var selected = false
-
-func _ready() -> void:
-	info_box.visible = false
+signal show_items
+signal hide_items
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "soul":
 		anim.play("select")
 		selected = true
+		emit_signal("show_items")
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "soul":
 		anim.play("default")
 		selected = false
-
-func _process(_delta: float) -> void:
-	if selected and global.current_state == global.state.PLAYER_TURN and Input.is_action_just_pressed("interact"):
-		toggle_item()
-
-func toggle_item():
-	info_box.visible = not info_box.visible
+		emit_signal("hide_items")

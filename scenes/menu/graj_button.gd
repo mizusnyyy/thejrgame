2621377800f324ec8cfe45b796dac7c0f76b04ed
mizek <91779178+mizusnyyy@users.noreply.github.com>
2025-08-08 14:07:40 +0200

    pp2

--- scenes/menu/graj_button.gd
+++ scenes/menu/graj_button.gd
@@ -1,7 +1,4 @@
 extends TextureButton
 
 func _on_pressed() -> void:
-	if MenuHandler.menu_instance:
-		MenuHandler.menu_instance.queue_free()
-		MenuHandler.menu_instance = null
 	get_tree().change_scene_to_file("res://scenes/story/pre-core/house.tscn")

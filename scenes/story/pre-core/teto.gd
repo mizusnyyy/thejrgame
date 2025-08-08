d8c3cb92a9214df1a22a0596193928d28220dc2a
P4w3lski <tvoipa2008@gmail.com>
2025-08-08 14:39:25 +0200

    ndn

--- scenes/story/pre-core/teto.gd
+++ scenes/story/pre-core/teto.gd
@@ -25,7 +25,7 @@ func _process(_delta):
 			if not dialog.dialogue_active:
 				can_talk = false
 				dialogf(pages[currentpage], portrait_texture[currentpage],typesound)
-				await dialog.dialogue_finisheds
+				await dialog.dialogue_finished
 			currentpage+=1
 		get_tree().change_scene_to_packed(preloadscena)
 

b947a0c9013ca1d4be11bf1236647d62182cb65d
Igoropolus <i.luniewski@outlook.com>
2025-08-10 16:34:50 +0200

    enemy_health_bar

---
+++ scenes/battle/Enemy_health_bar.gd
@@ -0,0 +1,55 @@
+extends TextureProgressBar
+@onready var ghost_bar = $"../Enemy__Ghost_bar"
+
+var textures = {
+	"green": preload("res://assets/sprite/battle/progress.png"),
+	"yellow": preload("res://assets/sprite/battle/progress_yellow.png"),
+	"red": preload("res://assets/sprite/battle/progress_red.png"),
+}
+
+var current_color = ""
+
+func _ready():
+	max_value = global.maxenemy_hp
+	ghost_bar.max_value = max_value
+	value = global.enemy_hp
+	_update_style(value)
+
+func _process(delta: float) -> void:
+	# upewnij się, że value jest aktualizowane
+	value = global.enemy_hp
+
+	if ghost_bar.value > value:
+		ghost_bar.value = lerp(ghost_bar.value, value, delta * 4.0)
+	else:
+		ghost_bar.value = value
+
+	if abs(ghost_bar.value - value) < 1.0:
+		ghost_bar.value = value
+
+	_update_style(value)
+
+func _update_style(health_value: float) -> void:
+
+	var color = ""
+	# proste, czytelne progi
+	if health_value > 50:
+		color = "green"
+	elif health_value > 30:
+		color = "yellow"
+	else:
+		color = "red"
+
+
+	# zabezpieczenie: sprawdź czy tekstura istnieje
+	if not textures.has(color) or textures[color] == null:
+		push_error("Brakuje tekstury dla koloru: " + color)
+		return
+
+	# jeśli inny kolor niż był — przypisz teksturę
+	if color != current_color:
+		# opcjonalnie: wyczyść i ustaw ponownie, żeby wymusić refresh
+		texture_progress = null
+		texture_progress = textures[color]
+		current_color = color
+		printt("texture_progress changed to:", color)
---
+++ scenes/battle/enemy_health_indicator_text.gd
@@ -0,0 +1,11 @@
+
+extends RichTextLabel	
+@onready var hp_bar = $"../Enemy_health_bar"
+
+func _process(delta: float) -> void:
+	hp_bar.value = global.enemy_hp
+	clear()
+	push_color(Color("#ffffff"))
+	append_text(("%d / %d"%[int(global.enemy_hp), int(global.maxenemy_hp)]))
+	pop()
+	

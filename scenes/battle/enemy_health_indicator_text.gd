035dec47fd6308cf54ccc97a88952923560e61ec
Igoropolus <i.luniewski@outlook.com>
2025-08-11 23:45:22 +0200

    mana_bar

--- scenes/battle/enemy_health_indicator_text.gd
+++ scenes/battle/enemy_health_indicator_text.gd
@@ -1,13 +1,13 @@
 
 extends RichTextLabel	
-@onready var hp_bar = $"../Enemy_health_bar"
+@onready var mana = $"../Mana__Ghost_bar"
 
 func _process(delta: float) -> void:
-	hp_bar.value = global.enemy_hp
+	mana.value = global.mana
 	clear()
 	push_color(Color("#ffffff"))
-	append_text(("%d / %d"%[int(global.enemy_hp), int(global.maxenemy_hp)]))
+	append_text(("%d / %d"%[int(global.mana), int(global.mana_max)]))
 	pop()
 	
-	if global.enemy_hp <= 0:
-		global.enemy_hp = 0
+	if global.mana <= 0:
+		global.mana = 0

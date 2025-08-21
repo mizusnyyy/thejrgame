d7cd94f7be653c3d5b36d9897903eb713d322041
Awruk1 <alanbaran08@gmail.com>
2025-08-21 17:32:06 +0200

    dzien dobry

--- scenes/ui/BounceEffect.gd
+++ scenes/ui/BounceEffect.gd
@@ -3,15 +3,18 @@ class_name BounceEffect
 
 var bbcode := "bounce"
 
-func _process_custom_fx(char_fx: CharFXTransform) -> bool:
-	var speed := 6.0
-	var height := 8.0
-	var delay_step := 0.08
-	var i := char_fx.range.x + char_fx.relative_index
-	var t := (Time.get_ticks_msec() / 1000.0) - (i * delay_step)
+# słownik z losowymi fazami dla liter
+var random_phase: Dictionary = {}
 
-	if t > 0:
-		char_fx.offset.y = -abs(sin(t * speed)) * height
-	else:
-		char_fx.offset.y = 0
+func _process_custom_fx(char_fx: CharFXTransform) -> bool:
+	var speed: float = 10.0
+	var height: float = 3.0
+	var i: int = char_fx.range.x + char_fx.relative_index
+
+	if not random_phase.has(i):
+		random_phase[i] = randf_range(0.0, PI * 2.0)
+
+	var t: float = Time.get_ticks_msec() / 1000.0
+	var phase: float = random_phase[i] as float
+	char_fx.offset.y = -abs(sin(t * speed + phase)) * height
 	return true

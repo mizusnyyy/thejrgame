64cc83e58d12370aeeeba332cfed7fdab17b9a1f
P4w3lski <tvoipa2008@gmail.com>
2025-09-17 21:01:34 +0200

    yeah!@

--- scenes/story/pre-core/quickchange.gd
+++ scenes/story/pre-core/quickchange.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 @export var namescene:String
+@export var diff:bool = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scenes/story/pre-core/"+namescene)
+		if diff:
+			get_tree().change_scene_to_file(namescene)
+		else:
+			get_tree().change_scene_to_file("res://scenes/story/pre-core/"+namescene)
 		musicsounds.play_music(preload("res://assets/sprite/intro/stevenafterpumming.wav"))

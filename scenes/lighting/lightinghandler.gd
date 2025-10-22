4e25c3c3cb176fe92e5ef4be249468ce610df901
mizek <91779178+mizusnyyy@users.noreply.github.com>
2025-10-22 21:19:29 +0200

    szyvbki fix

--- scenes/lighting/lightinghandler.gd
+++ scenes/lighting/lightinghandler.gd
@@ -5,8 +5,9 @@ var time=0.0 #godzina
 var time_flow:=true
 var time_speed:= 12.0 #ile godzin na sekunde
 var day=24
-var dawncolor=Color(1,7,0)
-var normalcolor=Color(0.5,0.6,0.9)
+var dawncolor = Color(254/255, 174/255, 0/255)      # złocisty wschód słońca
+var daycolor = Color(109/255, 149/255, 226/255)  # niebieski dzień
+var nightcolor = Color(0/255, 26/255, 51/255)       # ciemny nocny
 
 func _process(delta: float) -> void:
 	if time_flow:
--- scenes/lighting/timecolor.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var cm:=$CanvasModulate
-
-func _process(delta: float) -> void:
-	cm.color = Lighting.dawncolor.linear_interpolate(Lighting.normalcolor, Lighting.time / Lighting.day)
